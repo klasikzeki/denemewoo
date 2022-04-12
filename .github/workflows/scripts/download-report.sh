081b0babb6ca479aa237ff23621cedd274437f5a
rodel <rodel.calasagsag@automattic.com>
2022-04-12 21:56:52 +0800

    Script for downloading artifact

---
+++ .github/workflows/scripts/download-report.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+MAX_ATTEMPTS=10
+EXIT_CODE=0
+WAIT_TIME=10
+
+cd $DOWNLOAD_PATH
+
+for ((i = 1; i <= $MAX_ATTEMPTS; i++)); do
+
+    echo "Downloading $ARTIFACT_NAME (try #$i)..."
+    gh run download $RUN_ID \
+        --name $ARTIFACT_NAME \
+        --repo woocommerce/woocommerce
+
+    EXIT_CODE=$(echo $?)
+
+    if [[ $EXIT_CODE -eq 0 ]]; then
+        break
+    fi
+
+    if [[ $i -lt $MAX_ATTEMPTS ]]; then
+        echo "Retrying in $WAIT_TIME sec...\n\n"
+        sleep $WAIT_TIME
+    fi
+
+done
+
+exit $EXIT_CODE
