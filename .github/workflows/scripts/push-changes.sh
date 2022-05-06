f58e7615483f8fa1e777e2699b042df0feca2181
rodel <rodel.calasagsag@automattic.com>
2022-05-06 14:37:18 +0800

    Stop botwoo from referencing PR in commit message

--- .github/workflows/scripts/push-changes.sh
+++ .github/workflows/scripts/push-changes.sh
@@ -4,58 +4,53 @@
 # Pushes the newly generated report to the repo.
 #
 
-COMMIT_MESSAGE=""
+BOTWOO_COMMIT_MESSAGE=""
 EXIT_CODE=0
 MAX_ATTEMPTS=10
 
 set_commit_message() {
 
-    case $TEST_TYPE in
-    "e2e")
-        COMMIT_MESSAGE="Add E2E test report"
-        ;;
-    "api")
-        COMMIT_MESSAGE="Add API test report"
-        ;;
-    esac
-
     case $TEST_WORKFLOW in
     "daily")
-        COMMIT_MESSAGE="$COMMIT_MESSAGE - Daily smoke test"
+        BOTWOO_COMMIT_MESSAGE="Add test report - Daily smoke test"
         ;;
     "pr")
-        COMMIT_MESSAGE="$COMMIT_MESSAGE - PR woocommerce/woocommerce#$PR_NUMBER - Commit woocommerce/woocommerce@$COMMIT_SHA"
+        BOTWOO_COMMIT_MESSAGE="Add test report - PR #$PR_NUMBER - $COMMIT_MESSAGE"
         ;;
     "release")
-        COMMIT_MESSAGE="$COMMIT_MESSAGE - Release"
+        BOTWOO_COMMIT_MESSAGE="Add test report - Release"
         ;;
     esac
 }
 
+push_to_repo() {
+    cd $REPO_PATH
+    git config user.name $GH_USER
+    git config user.email $GH_EMAIL
+    git add .
+    git commit -m "$BOTWOO_COMMIT_MESSAGE" -m "Workflow run: https://github.com/woocommerce/woocommerce/actions/runs/$RUN_ID"
+
+    # Retry pushing changes up to 10 times when race conditions occur,
+    # like when multiple workflows are trying to push to the repo at the same time.
+    for ((i = 1; i <= $MAX_ATTEMPTS; i++)); do
+        echo "Attempting to push changes (try #$i)..."
+        git pull --rebase
+        git push "https://$GH_TOKEN@github.com/$GITHUB_REPOSITORY"
+        EXIT_CODE=$(echo $?)
+
+        if [[ $EXIT_CODE -eq 0 ]]; then
+            break
+        fi
+
+        if [[ $i -lt $MAX_ATTEMPTS ]]; then
+            echo "Retrying in 5 sec..."
+            sleep 5
+        fi
+    done
+}
+
 set_commit_message
 
-cd $REPO_PATH
-git config user.name $GH_USER
-git config user.email $GH_EMAIL
-git add .
-git commit -m "$COMMIT_MESSAGE" -m "Workflow run: https://github.com/woocommerce/woocommerce/actions/runs/$RUN_ID"
-
-# Retry pushing changes up to 10 times when race conditions occur,
-# like when multiple workflows are trying to push to the repo at the same time.
-for ((i = 1; i <= $MAX_ATTEMPTS; i++)); do
-    echo "Attempting to push changes (try #$i)..."
-    git pull --rebase
-    git push "https://$GH_TOKEN@github.com/$GITHUB_REPOSITORY"
-    EXIT_CODE=$(echo $?)
-
-    if [[ $EXIT_CODE -eq 0 ]]; then
-        break
-    fi
-
-    if [[ $i -lt $MAX_ATTEMPTS ]]; then
-        echo "Retrying in 5 sec..."
-        sleep 5
-    fi
-done
+push_to_repo
 
 exit $EXIT_CODE
