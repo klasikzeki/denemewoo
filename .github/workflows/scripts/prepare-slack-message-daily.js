f36aaeaf6c253e1de71820ac389795712323d34c
rodel <rodel.calasagsag@automattic.com>
2022-05-28 11:58:32 +0800

    Stringify slack message

--- .github/workflows/scripts/prepare-slack-message-daily.js
+++ .github/workflows/scripts/prepare-slack-message-daily.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
 const summary = require("../../../docs/daily/api/widgets/summary.json");
-const slackMessage = fs.readFileSync("./templates/SLACK_DAILY.md");
+const slackMessage = fs.readFileSync("./templates/SLACK_DAILY.md").toString();
 
 // MYTODO Place stats (from summary JSON) on slack message
 
-console.log(slackMessage.toString());
+console.log(slackMessage);
