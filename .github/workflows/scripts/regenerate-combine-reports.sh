a6db5074f33b0adad12f7bc1ef645f2f510c0250
rodel <rodel.calasagsag@automattic.com>
2022-05-29 21:53:30 +0800

    Prepare destination dir before copying

--- .github/workflows/scripts/regenerate-combine-reports.sh
+++ .github/workflows/scripts/regenerate-combine-reports.sh
@@ -14,6 +14,8 @@ if [[ -d "$REPO_ALLURE_REPORT_PATH" ]]; then
 else
 
     # Create the first report by copying the downloaded one.
+    mkdir -p $REPO_ALLURE_REPORT_PATH
+    mkdir -p $REPO_ALLURE_RESULTS_PATH
     cp -r $DOWNLOAD_ALLURE_REPORT_PATH $REPO_ALLURE_REPORT_PATH
     cp -r $DOWNLOAD_ALLURE_RESULTS_PATH $REPO_ALLURE_RESULTS_PATH
 
