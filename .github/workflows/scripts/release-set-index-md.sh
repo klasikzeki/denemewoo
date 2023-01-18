c83af8a880c7b8e5904476b6e20d55666c287478
rodelgc <rodel.calasagsag@automattic.com>
2023-01-19 01:56:16 +0800

    Add/dashboard release (#30)
    * Add steps to set index.md's and push changes
    
    * Add script to set index.md's
    
    * Update release index.md

---
+++ .github/workflows/scripts/release-set-index-md.sh
@@ -0,0 +1,27 @@
+#
+# Create or recreate the index.md file in PR test reports. 
+#
+#!/usr/bin/env bash
+
+INDEX_DIR="$GITHUB_WORKSPACE/repo/docs/release/$RELEASE_TAG/$ENV_DESCRIPTION/$TEST_TYPE"
+INDEX_PATH="$INDEX_DIR/index.md"
+LAST_PUBLISHED=$(date +'%Y-%m-%d %T %z')
+
+# Clear the directory of the index.md file.
+echo "Clearing \"$INDEX_DIR\"..."
+rm -rf "$INDEX_DIR"
+mkdir -p "$INDEX_DIR"
+
+# Write the contents (which is a Jekyll front matter) of the index.md file.
+echo "Writing contents to \"$INDEX_PATH\"..."
+echo "---" >"$INDEX_PATH"
+echo "layout: redirect" >>"$INDEX_PATH"
+echo "redirect_to: $S3_WEB_ENDPOINT/$S3_PATH/$TEST_TYPE/index.html" >>"$INDEX_PATH"
+echo "test_type: $TEST_TYPE" >>"$INDEX_PATH"
+echo "release_tag: $RELEASE_TAG" >>"$INDEX_PATH"
+echo "env_description: \"$ENV_DESCRIPTION\"" >>"$INDEX_PATH"
+echo "last_published: \"$LAST_PUBLISHED\"" >>"$INDEX_PATH"
+echo "---" >>"$INDEX_PATH"
+echo "Done!"
+echo "Contents of \"$INDEX_PATH\" is:"
+cat "$INDEX_PATH"
