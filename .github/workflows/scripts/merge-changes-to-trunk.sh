16152406b4547fa6d45c09763cf38993cf1493c2
rodelgc <rodel.calasagsag@automattic.com>
2023-01-25 12:07:28 +0800

    Fix/no change (#119)
    * Gracefully exit when nothing to commit
    
    * Print commit_result
    
    * Verify exit code too
    
    * Fix command

--- .github/workflows/scripts/merge-changes-to-trunk.sh
+++ .github/workflows/scripts/merge-changes-to-trunk.sh
@@ -11,11 +11,18 @@ git config user.email $GITHUB_EMAIL
 # Create new branch.
 git checkout -b $BRANCH_NAME
 
-# Commit & push changes
+# Commit changes
 git add .
-git commit -m "$BOT_COMMIT_MESSAGE" -m "Workflow run: https://github.com/woocommerce/woocommerce/actions/runs/$RUN_ID"
-git push --set-upstream origin $BRANCH_NAME
+commit_result=$(git commit -m "$BOT_COMMIT_MESSAGE" -m "Workflow run: https://github.com/woocommerce/woocommerce/actions/runs/$RUN_ID")
+commit_exit_code=$(echo $?)
+echo $commit_result
 
-# Create PR and merge
+# Gracefully exit when there's nothing to commit
+if [[ $commit_result == *"nothing to commit"* && $commit_exit_code != 0 ]]
+    then exit 0;
+fi
+
+# If there are changes, create PR and merge
+git push --set-upstream origin $BRANCH_NAME
 gh pr create --title "$BOT_COMMIT_MESSAGE" --body ""
 gh pr merge --delete-branch --squash
