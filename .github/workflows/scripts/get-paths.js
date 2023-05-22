79b35fd9ef4e475e98b66668208331cf9cd5695b
rodelgc <rodel.calasagsag@automattic.com>
2023-05-22 15:59:23 +0800

    Do not normalize paths (#1325)

--- .github/workflows/scripts/get-paths.js
+++ .github/workflows/scripts/get-paths.js
@@ -1,14 +1,13 @@
 module.exports = ({ core }) => {
   const { GITHUB_WORKSPACE, REPORT_URL, S3_WEB_ENDPOINT } = process.env;
-  const path = require('path');
 
   const dashboardHome =
     'https://woocommerce.github.io/woocommerce-test-reports/';
   const localHome = `${GITHUB_WORKSPACE}/docs/`;
   const s3WebHome = `${S3_WEB_ENDPOINT}/public/`;
 
-  const localDir = path.normalize(REPORT_URL.replace(dashboardHome, localHome));
-  const s3WebDir = path.normalize(REPORT_URL.replace(dashboardHome, s3WebHome));
+  const localDir = REPORT_URL.replace(dashboardHome, localHome);
+  const s3WebDir = REPORT_URL.replace(dashboardHome, s3WebHome);
 
   core.setOutput('local-dir', localDir);
   core.setOutput('s3-web-dir', s3WebDir);
