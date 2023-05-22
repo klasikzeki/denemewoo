7f89d78d8846cfad1a9525ba42dbe52b9cbcf631
rodelgc <rodel.calasagsag@automattic.com>
2023-05-22 10:00:45 +0800

    Publish PR reports v2
    * Update workflow to add PR reports

---
+++ .github/workflows/scripts/create-index-pr.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Configure git username and email
+cd $GITHUB_WORKSPACE
+git config user.name $GITHUB_USER
+git config user.email $GITHUB_EMAIL
+
+# Create index.md
+mkdir -p $DIR_PATH
+cd $DIR_PATH
+LAST_PUBLISHED=$(date +'%Y-%m-%d %T %z')
+PR_TITLE_ENCODED=$(gh pr view $PR_NUMBER --repo woocommerce/woocommerce --json title --jq '.title|@uri')
+echo "---" > index.md
+echo "layout: redirect" >> index.md
+echo "redirect_to: $REPORT_URL/index.html" >> index.md
+echo "pr_number: $PR_NUMBER" >> index.md
+echo "pr_title_encoded: \"$PR_TITLE_ENCODED\"" >> index.md
+echo "pr_test_type: $TEST_TYPE" >> index.md
+echo "last_published: \"$LAST_PUBLISHED\"" >> index.md
+echo "---" >> index.md
+
+# Commit changes to new branch
+cd $GITHUB_WORKSPACE
+git checkout -b $BRANCH
+git add .
+git commit -m "$BOT_COMMIT_MESSAGE"
+
+# Create PR and merge
+git push --set-upstream origin $BRANCH
+gh pr merge --delete-branch --squash
+
+# Echo exit code to GitHub action
+exit_code=$(echo $?)
+exit $exit_code
